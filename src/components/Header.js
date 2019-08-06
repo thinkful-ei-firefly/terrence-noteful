057ea3ba19eacc5a4fed334c3d4a553e46df3ac1
Ramarduk <ramarduk@gmail.com>
2019-08-06 09:53:41 -0600

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../app/App.css';
+
+export default function Header() {
+    return (
+        <div className="App-header">
+            <h1>Noteful</h1>
+        </div>
+    )
+}
