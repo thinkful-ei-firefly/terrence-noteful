7aa0a08f554ac3d1019c2ce11cf8b88917e8b522
Ramarduk <ramarduk@gmail.com>
2019-08-06 11:40:55 -0600

    installing sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../app/App.css';
+
+
+export default function Sidebar() {
+    return (
+        <div className='Sidebar'>
+            <p>it works</p>
+        </div>
+    )
+}
